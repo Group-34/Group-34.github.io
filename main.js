f53396e6f19e25598efb5fa8b77a4c6d4a6d5f16
Benjamin Sayyeau <bsayy072@uottawa.ca>
2021-10-24 14:43:10 -0400

    finally_javascript

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 
-document.getElementById("namebox").onchange = function() {myFunction()};
-function myFunction() {
-    var x = document.getElementById("namebox");
-    x.value = x.value.toUpperCase();
+document.getElementById("namebox").onchange = function() {myFunction("namebox","request1")};
+document.getElementById("emailbox").onchange = function() {myFunction("emailbox","request2")};
+document.getElementById("subjectbox").onchange = function() {myFunction("subjectbox","request3")};
+document.getElementById("messbox").onchange = function() {myFunction("messbox","request4")};
+
+function myFunction(boxid,requestnum) {
+    var x = document.getElementById(boxid);
+    if (x && x.value) {
+        document.getElementById(requestnum).innerHTML = "";
+    }
 }
